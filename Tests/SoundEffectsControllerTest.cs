9cdcc5f17557b46a877445813e9fc57430c63755
GabrielUzel <gabriel.uzel1@gmail.com>
2025-04-14 17:06:40 -0300

    Organized sprites and scripts in folders

---
+++ Tests/SoundEffectsControllerTest.cs
@@ -0,0 +1,13 @@
+namespace Tests;
+
+public class SoundEffectsControllerTest {
+    [SetUp]
+    public void Setup() {
+    
+    }
+
+    [Test]
+    public void toggleMusicTest() {
+        
+    }
+}
