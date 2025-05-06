73c9ceb88cb3691efeeacea59d9f10a31d282b8e
anabeatrizsse1 <anabeatrizbrsp@gmail.com>
2025-05-05 23:24:34 -0300

    Corrigindo um bug

--- Assets/Scripts/PanelPopUp/ButtonPopUp.cs
+++ Assets/Scripts/PanelPopUp/ButtonPopUp.cs
@@ -2,17 +2,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonPopUp : MonoBehaviour
-{
-    public void BackHome()
+{    public void BackHome()
     {
         SceneManager.LoadScene("Main_Menu_Scene");
     }
 
     
 public void BackButton(){
-
+    SceneManager.LoadScene("");
 }
 public void StartButton(){
-    
+    SceneManager.LoadScene("");
 }
 }
