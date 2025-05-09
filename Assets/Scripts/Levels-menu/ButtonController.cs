c2a6d63ceddc29162a2bb86a90ab198d86e195a5
Camila-Miranda <camilasilveirabm@gmail.com>
2025-05-08 23:07:03 -0300

    Correção da imagem dos botẽs

--- Assets/Scripts/Levels-menu/ButtonController.cs
+++ Assets/Scripts/Levels-menu/ButtonController.cs
@@ -10,48 +10,42 @@ public class ButtonController : MonoBehaviour
     public Button LevelFour;
     public Button LevelFive;
 
+    private Image ImageButtonTwo;
+    private Image ImageButtonThree;
+    private Image ImageButtonFour;
+    private Image ImageButtonFive;
+
     void Start()
     {
+        ImageButtonTwo = LevelTwo.GetComponent<Image>();
+        ImageButtonThree = LevelThree.GetComponent<Image>();
+        ImageButtonFour = LevelFour.GetComponent<Image>();
+        ImageButtonFive = LevelFive.GetComponent<Image>();
+
         LevelTwo.interactable = false;
-        LevelThree.interactable = false; 
-        LevelFour.interactable = false; 
-        LevelFive.interactable = false;  
+        LevelThree.interactable = false;
+        LevelFour.interactable = false;
+        LevelFive.interactable = false;
 
-        if(Level == 2){
+        if (Level >= 2)
+        {
             LevelTwo.interactable = true;
-            LevelThree.interactable = false; 
-            LevelFour.interactable = false; 
-            LevelFive.interactable = false;  
-            Image ImageButton = LevelTwo.GetComponent<Image>();
-            ImageButton.color = new Color(1f, 1f, 1f, 0f);
+            ImageButtonTwo.color = new Color(1f, 1f, 1f, 0f);
         }
-
-        if(Level == 3){
-            LevelTwo.interactable = true;
-            LevelThree.interactable = true; 
-            LevelFour.interactable = false; 
-            LevelFive.interactable = false;  
-            Image ImageButton = LevelThree.GetComponent<Image>();
-            ImageButton.color = new Color(1f, 1f, 1f, 0f);
+        if (Level >= 3)
+        {
+            LevelThree.interactable = true;
+            ImageButtonThree.color = new Color(1f, 1f, 1f, 0f);
         }
-
-        if(Level == 4){
-            LevelTwo.interactable = true;
-            LevelThree.interactable = true; 
-            LevelFour.interactable = true; 
-            LevelFive.interactable = false;              
-            Image ImageButton = LevelFour.GetComponent<Image>();
-            ImageButton.color = new Color(1f, 1f, 1f, 0f);
+        if (Level >= 4)
+        {
+            LevelFour.interactable = true;
+            ImageButtonFour.color = new Color(1f, 1f, 1f, 0f);
         }
-
-        if(Level == 5){
-            LevelTwo.interactable = true;
-            LevelThree.interactable = true; 
-            LevelFour.interactable = true; 
-            LevelFive.interactable = true;              
-            Image ImageButton = LevelFive.GetComponent<Image>();
-            ImageButton.color = new Color(1f, 1f, 1f, 0f);
+        if (Level >= 5)
+        {
+            LevelFive.interactable = true;
+            ImageButtonFive.color = new Color(1f, 1f, 1f, 0f);
         }
-
     }
 }
