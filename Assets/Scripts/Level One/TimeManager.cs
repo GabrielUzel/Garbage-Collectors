9654bc9bf3ff99f7a151b6b5fe51a98a30105175
adryalves <adryellenalves09@gmail.com>
2025-05-14 22:33:40 -0300

    added timer and renamed wastes to english

---
+++ Assets/Scripts/Level One/TimeManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Level_One
+{
+    public class TimeManager : MonoBehaviour
+    {
+        public float timeRemaining = 150;
+        public bool timerIsRunning = false;
+        [SerializeField] TextMeshProUGUI timeText;
+
+
+        private void Start()
+        {
+
+            timerIsRunning = true;
+        }
+
+        public void DisplayTime(float timeToDisplay)
+        {
+            timeToDisplay += 1;
+
+            float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+            float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+            timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+
+        void Update()
+        {
+            if (timerIsRunning)
+            {
+                if (timeRemaining > 0)
+                {
+                    timeRemaining -= Time.deltaTime;
+                    DisplayTime(timeRemaining);
+                }
+                else
+                {
+                    // aqui vou inserir pra ver se o usuario ganhou ou nao e chamar as proximas telas
+                    Debug.Log("Time has run out!");
+                    timeRemaining = 0;
+                    timerIsRunning = false;
+                }
+            }
+        }
+    }
+}
