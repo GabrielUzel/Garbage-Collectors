160878ee7d31c2fd55aa05d5eb29b5cf529a1ae1
Camila-Miranda <camilasilveirabm@gmail.com>
2025-06-05 01:17:35 -0300

    criação do script da criptografia do arquivo json

---
+++ Assets/Scripts/Data Persistence/Save File/Encryption.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class Encryption
+{
+    private static readonly string key = "mwpjgld9y46aeswp"; 
+
+    public static string Encrypt(string plainText)
+    {
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = keyBytes;
+            aes.GenerateIV();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                ms.Write(aes.IV, 0, aes.IV.Length); 
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                using (StreamWriter sw = new StreamWriter(cs))
+                {
+                    sw.Write(plainText);
+                }
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+    }
+
+    public static string Decrypt(string encryptedText)
+    {
+        byte[] fullData = Convert.FromBase64String(encryptedText);
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = keyBytes;
+
+            byte[] iv = new byte[aes.BlockSize / 8];
+            Array.Copy(fullData, 0, iv, 0, iv.Length);
+            aes.IV = iv;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                ms.Write(fullData, iv.Length, fullData.Length - iv.Length);
+                ms.Position = 0;
+
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                using (StreamReader sr = new StreamReader(cs))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+        }
+    }
+}
