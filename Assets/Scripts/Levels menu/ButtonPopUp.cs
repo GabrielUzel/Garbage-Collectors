a4c8b7a150c1fe1cd069f698bd73fe6856704fd1
GabrielUzel <gabriel.uzel1@gmail.com>
2025-06-27 20:59:22 -0300

    Fixed mute button on menu not changing sprite

--- Assets/Scripts/Levels menu/ButtonPopUp.cs
+++ Assets/Scripts/Levels menu/ButtonPopUp.cs
@@ -4,9 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class ButtonPopUp : MonoBehaviour
 {
-    public void StartButton(Text LevelText)
-    {
-        Debug.Log(LevelText.text);
-        SceneManager.LoadScene("Level_One_Scene");
-    }
+  public void StartButton(Text LevelText)
+  {
+    SceneManager.LoadScene("Level_One_Scene");
+  }
 }
