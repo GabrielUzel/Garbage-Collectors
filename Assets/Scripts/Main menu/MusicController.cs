828db7abab17a74630ef995c53fcc59be0ee29e2
GabrielUzel <gabriel.uzel1@gmail.com>
2025-06-28 20:18:21 -0300

    Update music button controllers

--- Assets/Scripts/Main menu/MusicController.cs
+++ Assets/Scripts/Main menu/MusicController.cs
@@ -1,12 +1,8 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MusicController : MonoBehaviour
 {
   public static MusicController Instance;
-  public Sprite musicOff;
-  public Sprite musicOn;
-  public Button musicButton;
   private bool isMuted = false;
   private const string musicPrefKey = "MusicMuted";
 
@@ -25,19 +21,12 @@ public class MusicController : MonoBehaviour
   void Start()
   {
     isMuted = PlayerPrefs.GetInt(musicPrefKey, 0) == 1;
-    updateUI(isMuted);
   }
 
-  public void updateUI(bool isMuted)
-  {
-    musicButton.image.sprite = isMuted ? musicOff : musicOn;
-  }
-
-  public void toggleMusic()
+  public void ToggleMusic()
   {
     isMuted = !isMuted;
     BackgroundMusic.Instance.SetMusicMute(isMuted);
-    updateUI(isMuted);
 
     PlayerPrefs.SetInt(musicPrefKey, isMuted ? 1 : 0);
     PlayerPrefs.Save();
@@ -51,5 +40,10 @@ public class MusicController : MonoBehaviour
   public void IncreaseMusicVolume()
   {
     BackgroundMusic.Instance.SetMusicVolume(1f);
+  }
+
+  public bool GetIsMuted()
+  {
+    return isMuted;
   }
 }
